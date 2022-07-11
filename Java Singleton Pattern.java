e0ab58d263cae9b4ee73c69ff4fb31e7ea81481b
EmanuelButoiGit <emanuelb1000@gmail.com>
2022-07-11 20:16:26 +0300

    Create Java Singleton Pattern.java

---
+++ Java Singleton Pattern.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+import java.lang.reflect.*;
+
+
+class Singleton{
+    
+    public String str;
+    private static Singleton singleton;
+    private Singleton(){
+        
+    }
+    
+    public static Singleton getSingleInstance(){
+        if(singleton == null){
+            singleton = new Singleton();
+        }
+        return singleton;
+    }
+    
+};
