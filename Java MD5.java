78fe9bf79ef4ab167858bf0471276caeee078074
EmanuelButoiGit <emanuelb1000@gmail.com>
2022-07-16 20:38:19 +0300

    Create Java MD5.java

---
+++ Java MD5.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+public class Solution {
+
+    public static void main(String[] args) throws NoSuchAlgorithmException {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        /* Read and save the input String */
+        Scanner scan = new Scanner(System.in);
+        String str = scan.next();
+        
+        // Encode the String using MD5 
+        MessageDigest md = MessageDigest.getInstance("MD5");
+        md.update(str.getBytes());
+        byte[] digest = md.digest();
+        
+        // Print the encoded value in hexadecimal 
+        for (byte b : digest) {
+            System.out.format("%02x", b);
+        }
+    }
+}
